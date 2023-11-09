05bce26b9d4efac05ef98bf66bf52dc6fc46f848
Chloé Lourseyre <chloe.lourseyre@trust-in-soft.com>
2023-11-09 09:14:11 +0100

    Update on Chapter07

--- Chapter07/client.cpp
+++ Chapter07/client.cpp
@@ -6,33 +6,33 @@ static const char *const SERVER_HOST = "127.168.0.1";
 static const int MAX_ITERATIONS = 4;
 
 int
-main (int argc, char *argv[])
+main(int argc, char *argv[])
 {
     const char *server_host = argc > 1 ? argv[1] : SERVER_HOST;
-    u_short server_port = argc > 2 ? ACE_OS::atoi (argv[2]) : SERVER_PORT;
-    int max_iterations = argc > 3 ? ACE_OS::atoi (argv[3]) : MAX_ITERATIONS;
+    u_short server_port = argc > 2 ? ACE_OS::atoi(argv[2]) : SERVER_PORT;
+    int max_iterations = argc > 3 ? ACE_OS::atoi(argv[3]) : MAX_ITERATIONS;
 
     ACE_SOCK_Stream server;
 
     ACE_SOCK_Connector connector;
 
-    ACE_INET_Addr addr (server_port, server_host);
+    ACE_INET_Addr addr(server_port, server_host);
 
-    if (connector.connect (server, addr) == -1)
-        ACE_ERROR_RETURN ((LM_ERROR,"%p\n","open"),-1);
+    if (connector.connect(server, addr) == -1)
+        ACE_ERROR_RETURN((LM_ERROR,"%p\n","open"),-1);
 
     for (int i = 0; i < max_iterations; i++)
         {
             char buf[BUFSIZ];
 
-            ACE_OS::sprintf (buf,"message = %d\n",i + 1);
-            if (server.send_n (buf,ACE_OS::strlen (buf)) == -1)
-                ACE_ERROR_RETURN ((LM_ERROR,"%p\n","send"),-1);
+            ACE_OS::sprintf(buf,"message = %d\n",i + 1);
+            if (server.send_n(buf,ACE_OS::strlen(buf)) == -1)
+                ACE_ERROR_RETURN((LM_ERROR,"%p\n","send"),-1);
             else
-                ACE_OS::sleep (1);
+                ACE_OS::sleep(1);
         }
 
-    if (server.close () == -1)
-        ACE_ERROR_RETURN ((LM_ERROR,"%p\n","close"),-1);
+    if (server.close() == -1)
+        ACE_ERROR_RETURN((LM_ERROR,"%p\n","close"),-1);
     return 0;
 }
--- Chapter07/server.cpp
+++ Chapter07/server.cpp
@@ -11,18 +11,18 @@ ACE_Reactor *g_reactor;
 class Logging_Handler : public ACE_Event_Handler
 {
 public:
-    int open (ACE_Reactor *reactor)
+    int open(ACE_Reactor *reactor)
     {
         reactor_ = reactor;
 
-        if (reactor->register_handler (this, ACE_Event_Handler::READ_MASK) == -1)
-            ACE_ERROR_RETURN ((LM_ERROR, "(%P|%t) can't register with reactor\n"), -1);
+        if (reactor->register_handler(this, ACE_Event_Handler::READ_MASK) == -1)
+            ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) can't register with reactor\n"), -1);
         return 0;
     }
 
-    int close (void)
+    int close(void)
     {
-        return this->handle_close (ACE_INVALID_HANDLE, ACE_Event_Handler::RWE_MASK);
+        return this->handle_close(ACE_INVALID_HANDLE, ACE_Event_Handler::RWE_MASK);
     }
 
     operator ACE_SOCK_Stream &()
@@ -30,43 +30,42 @@ public:
         return this->cli_stream_;
     }
 
-protected:
+private:
 
-    ACE_HANDLE get_handle (void) const
+    ACE_HANDLE get_handle(void) const
     {
-        return this->cli_stream_.get_handle ();
+        return this->cli_stream_.get_handle();
     }
 
 
-    virtual int handle_input (ACE_HANDLE)
+    virtual int handle_input(ACE_HANDLE)
     {
         char buf[BUFSIZ + 1];
 
         ssize_t retval;
-        switch (retval = this->cli_stream_.recv (buf, BUFSIZ))
+        switch(retval = this->cli_stream_.recv(buf, BUFSIZ))
         {
         case -1:
-            ACE_ERROR_RETURN ((LM_ERROR, "(%P|%t) %p bad read\n", "client logger"), -1);
+            ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p bad read\n", "client logger"), -1);
         case 0:
-            ACE_ERROR_RETURN ((LM_ERROR, "(%P|%t) closing log daemon (fd = %d)\n", this->get_handle ()), -1);
+            ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) closing log daemon(fd = %d)\n", this->get_handle()), -1);
         default:
             buf[retval] = '\0';
-            ACE_DEBUG ((LM_DEBUG, "(%P|%t) from client: %s", buf));
+            ACE_DEBUG((LM_DEBUG, "(%P|%t) from client: %s", buf));
         }
 
         return 0;
     }
 
-    int handle_close (ACE_HANDLE, ACE_Reactor_Mask _mask)
+    int handle_close(ACE_HANDLE, ACE_Reactor_Mask _mask)
     {
-        reactor_->remove_handler (this, _mask | ACE_Event_Handler::DONT_CALL);
-        cli_stream_.close ();
+        reactor_->remove_handler(this, _mask | ACE_Event_Handler::DONT_CALL);
+        cli_stream_.close();
         delete this;
 
         return 0;
     }
 
-protected:
     ACE_SOCK_Stream cli_stream_;
     ACE_Reactor *reactor_;
 };
@@ -75,38 +74,36 @@ protected:
 class Logging_Acceptor : public ACE_Event_Handler
 {
 public:
-    int open (const ACE_INET_Addr &addr, ACE_Reactor *reactor)
+    int open(const ACE_INET_Addr &addr, ACE_Reactor *reactor)
     {
-        if (this->peer_acceptor_.open (addr, 1) == -1)
+        if (this->peer_acceptor_.open(addr, 1) == -1)
             return -1;
 
         reactor_ = reactor;
 
-        return reactor->register_handler (this, ACE_Event_Handler::ACCEPT_MASK);
+        return reactor->register_handler(this, ACE_Event_Handler::ACCEPT_MASK);
     }
 
 private:
-    ACE_HANDLE get_handle (void) const
+    ACE_HANDLE get_handle(void) const
     {
-        return this->peer_acceptor_.get_handle ();
+        return this->peer_acceptor_.get_handle();
     }
 
-    virtual int handle_input (ACE_HANDLE handle)
+    virtual int handle_input(ACE_HANDLE unused)
     {
-        ACE_UNUSED_ARG (handle);
         Logging_Handler *svc_handler;
-        ACE_NEW_RETURN (svc_handler,Logging_Handler, -1);
+        ACE_NEW_RETURN(svc_handler,Logging_Handler, -1);
 
-        if (this->peer_acceptor_.accept (*svc_handler) == -1)
-            ACE_ERROR_RETURN ((LM_ERROR, "%p", "accept failed"), -1);
+        if (this->peer_acceptor_.accept(*svc_handler) == -1)
+            ACE_ERROR_RETURN((LM_ERROR, "%p", "accept failed"), -1);
 
-        if (svc_handler->open (reactor_) == -1)
-            svc_handler->close ();
+        if (svc_handler->open(reactor_) == -1)
+            svc_handler->close();
 
         return 0;
     }
 
-protected:
     ACE_SOCK_Acceptor peer_acceptor_;
     ACE_Reactor *reactor_;
 };
@@ -114,20 +111,19 @@ protected:
 
 static const u_short PORT = ACE_DEFAULT_SERVER_PORT;
 
-int main (int, char *[])
+int main(int, char *[])
 {
-    ACE_NEW_RETURN (g_reactor,ACE_Reactor,1);
-    ACE_INET_Addr addr (PORT);
-    Logging_Acceptor *peer_acceptor;
-    ACE_NEW_RETURN (peer_acceptor,Logging_Acceptor,1);
+    ACE_Reactor g_reactor;
+    ACE_INET_Addr addr(PORT);
+    Logging_Acceptor peer_acceptor;
 
-    if (peer_acceptor->open (addr, g_reactor) == -1 )
-        ACE_ERROR_RETURN ((LM_ERROR, "Opening Acceptor\n"),-1);
+    if (peer_acceptor.open(addr, &g_reactor) == -1 )
+        ACE_ERROR_RETURN((LM_ERROR, "Opening Acceptor\n"),-1);
 
-    ACE_DEBUG ((LM_DEBUG,"(%P|%t) starting up server logging daemon\n"));
+    ACE_DEBUG((LM_DEBUG,"(%P|%t) starting up server logging daemon\n"));
 
     while (1)
-        g_reactor->handle_events ();
+        g_reactor.handle_events();
 
     return 0;
 }
