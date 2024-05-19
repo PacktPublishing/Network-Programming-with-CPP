94a37ac7f5da0fcd6f2c65fca8d141f9e0ca2a79
Senua <chloe.lourseyre@gmail.com>
2024-05-19 15:56:06 +0200

    Update client.cpp

--- Chapter09/client.cpp
+++ Chapter09/client.cpp
@@ -3,8 +3,6 @@
 #include <sstream>
 #include <boost/network/protocol/http/client.hpp>
 
-namespace http = boost::network::http;
-
 int main(int argc, char * argv[])
 {
     if (argc != 3) 
@@ -21,10 +19,10 @@ int main(int argc, char * argv[])
         std::ostringstream full_url;
         full_url << "http://" << address << ":" << port << "/";
         
-        http::client::request request(full_url.str());
+        boost::network::http::client::request request(full_url.str());
         
-        http::client client;
-        http::client::response response = client.get(request);
+        boost::network::http::client client;
+        boost::network::http::client::response response = client.get(request);
         
         std::cout << body(response) << std::endl;
     }
