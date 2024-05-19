97fa1d2010dc88016e458dbb17f2153bfcd04674
Senua <chloe.lourseyre@gmail.com>
2024-05-19 15:54:57 +0200

    Update client.cpp

--- Chapter09/client.cpp
+++ Chapter09/client.cpp
@@ -1,25 +1,35 @@
-#include <boost/network/protocol/http/client.hpp>
+#include <iostream>
 #include <string>
 #include <sstream>
-#include <iostream>
+#include <boost/network/protocol/http/client.hpp>
 
 namespace http = boost::network::http;
 
-int main(int argc, char * argv[]) {
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " address port" << std::endl;
+int main(int argc, char * argv[])
+{
+    if (argc != 3) 
+    {
+        std::cout << "Usage: " << argv[0] << " address port" << std::endl;
         return 1;
     }
 
-    try {
+    std::string address = argv[1];
+    std::string port = argv[2];
+
+    try 
+    {
+        std::ostringstream full_url;
+        full_url << "http://" << address << ":" << port << "/";
+        
+        http::client::request request(full_url.str());
+        
         http::client client;
-        std::ostringstream url;
-        url << "http://" << argv[1] << ":" << argv[2] << "/";
-        http::client::request request(url.str());
-        http::client::response response =
-            client.get(request);
+        http::client::response response = client.get(request);
+        
         std::cout << body(response) << std::endl;
-    } catch (std::exception & e) {
+    }
+    catch (std::exception & e) 
+    {
         std::cerr << e.what() << std::endl;
         return 1;
     }
